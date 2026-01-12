c429a39af6c7ef4798d1e8a0acf1d5b1e3c2eaa9
SloppyG33k <aimeeclark726@gmail.com>
2026-01-13 00:47:47 +0800

    all the backend and frontend part

---
+++ backend/ft_fashion_backend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from products.api import ProductViewSet
+
+router = routers.DefaultRouter()
+router.register(r'products', ProductViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
---
+++ backend/orders/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from products.models import Product
+import uuid
+
+class Order(models.Model):
+    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    customer_email = models.EmailField()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+
+
+
